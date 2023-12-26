190aae84c199129cb8702f5996aa3ccb584974cb
amyshaqistina <amyshaqistina.amerolazuam@live.iium.edu.my>
2023-12-26 20:28:21 +0800

    updated by Amysha

--- DiscountProduct.java
+++ DiscountProduct.java
@@ -1,5 +1,16 @@
-public class DiscountProduct {
-    
-    private double discount;
+public class DiscountProduct extends Product {
 
+    public DiscountProduct(int itemNo, String description, double price) {
+        super(itemNo, description, price);
+    }
+
+    @Override
+    public double applyDiscount() {
+        double discount = 0;
+        if (super.getTotal() > 20) {
+            discount = super.getTotal() * 0.25; // 25% discount if total is more than $20
+            super.setTotal(super.getTotal() - discount);
+        }
+        return discount;
+    }
 }
--- PointOfSale.java
+++ PointOfSale.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class PointOfSale {
     private static final double GST = 0.06;
+    private DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
-    public void processPayment(double amountPaid, double totalAmount) {
-        if (amountPaid >= totalAmount) {
-            double change = amountPaid - totalAmount;
-            System.out.println("Payment successful. Change: $" + change);
-            printReceipt(totalAmount);
+    public void CalculateTotal(double totalAmount, double totalDiscount) {
+        Scanner scanner = new Scanner(System.in);
+        double totalAfterDis = totalAmount - totalDiscount;
+        double grandTotal = totalAfterDis + (totalAfterDis * GST);
+
+        System.out.println("Total amount: RM" + decimalFormat.format(totalAmount));
+        System.out.println("Total amount after discount: RM" + decimalFormat.format(totalAfterDis));
+        System.out.println("Total amount (incl. GST): RM" + decimalFormat.format(grandTotal));
+
+        System.out.print("Enter amount paid: RM");
+        double amountPaid = scanner.nextDouble();
+        processPayment(amountPaid, grandTotal);
+    }
+
+    public void processPayment(double amountPaid, double grandTotal) {
+        if (amountPaid >= grandTotal) {
+            double change = amountPaid - grandTotal;
+            System.out.println("Payment successful. Change: RM" + decimalFormat.format(change));
+            printReceipt(grandTotal);
         } else {
             System.out.println("Insufficient payment. Please pay the full amount.");
         }
     }
-    public void printReceipt(double totalAmount) {
-        System.out.println("Thank you for shopping with us!");
-        System.out.println("Total amount: $" + totalAmount);
-        System.out.println("GST: $" + totalAmount * GST);
-        System.out.println("Total amount (incl. GST): $" + totalAmount * (1 + GST));
-    }
 
+    public void printReceipt(double grandTotal) {
+        System.out.println("Thank you for shopping with us!");
+        System.out.println("=============================================");
+        System.out.println("Total amount (incl. GST): RM" + decimalFormat.format(grandTotal));
+    }
 }
