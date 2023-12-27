e2e5defd33db7cad03d8a747d8137798928d6082
nnurfathinatirah <148221756+nnurfathinatirah@users.noreply.github.com>
2023-12-27 22:12:44 +0800

    update

--- DiscountProduct.java
+++ DiscountProduct.java
@@ -1,5 +1,28 @@
-public class DiscountProduct {
-    private String product;
-    private double discount;
+public class DiscountProduct extends Product {
 
+    public DiscountProduct(int itemNo, String description, double price) {
+        super(itemNo, description, price);
+    }
+
+    @Override
+    public double applyDiscount() {
+        double discount = 0;
+        if (super.getTotal() > 20) {
+            discount = super.getTotal() * 0.25; // 25% discount if total is more than $20
+            super.setTotal(super.getTotal() - discount);
+        }
+        return discount;
+    }
+
+    // Getter for total 
+    public double getTotal() {
+        return super.getTotal();
+    }
+
+    // Setter for total
+    public void setTotal(double total) {
+        super.setTotal(total);
+    }
+
+    
 }
